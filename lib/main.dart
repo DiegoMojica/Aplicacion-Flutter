7c5d3a39f34def66a158026dcfadd1eaab49807e
DiegoMojica <diegomojica261@gmail.com>
2023-01-10 18:08:13 -0500

    usuario y contraseña

--- lib/main.dart
+++ lib/main.dart
@@ -25,34 +25,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: ListView(
         children: <Widget>[
           Container(
             width: double.infinity,
             padding: EdgeInsets.symmetric(vertical: 0),
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment(0.8, 1),
-                colors: <Color>[
-                  Color.fromARGB(255, 105, 105, 105),
-                  Color.fromARGB(255, 211, 210, 210),
-                ],
-              ),
-            ),
             child: Image.asset("assets/pensador.webp"),
             height: 300,
           ),
           Center(
               child: Card(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 260),
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 60),
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
-                    decoration: InputDecoration(labelText: "Usuario:"),
+                      decoration: InputDecoration(labelText: "Usuario")),
+                  SizedBox(height: 40),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "Contraseña"),
+                    obscureText: true,
                   )
                 ],
               ),
